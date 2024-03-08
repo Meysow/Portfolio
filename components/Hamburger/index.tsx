9d41d8b7ebc1049d8d76744bf5acabbf38174b78
Thibault D <thibault.dilman@tuta.io>
2024-03-08 13:41:48 +0100

    added an highlight to which location you are in for the nav links

--- components/Hamburger/index.tsx
+++ components/Hamburger/index.tsx
@@ -1,54 +1,58 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import styles from "./Hamburger.module.scss";
 
 const Hamburger = () => {
-  const [active, setActive] = useState<boolean>(false);
-  const isActive = active ? styles.active : styles.notActive;
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const isModalOpen = modalOpen ? styles.modalOpen : styles.modalClosed;
 
-  // TODO: animer la sortie de la modale
+  const pathname = usePathname();
+  const isActiveLink = (linkPath: string): string => {
+    return pathname === linkPath ? styles.activeLink : "";
+  };
 
   return (
     <>
       <div className={styles.hamburger}>
         <div
-          className={`${styles.btn} ${isActive}`}
-          onClick={() => setActive((prev) => !prev)}
+          className={`${styles.btn} ${isModalOpen}`}
+          onClick={() => setModalOpen((prev) => !prev)}
         >
           <span></span>
           <span></span>
           <span></span>
         </div>
       </div>
-      <div className={`${styles.modal} ${isActive}`}>
+      <div className={`${styles.modal} ${isModalOpen}`}>
         <div
-          className={`${styles.btn} ${isActive}`}
-          onClick={() => setActive((prev) => !prev)}
+          className={`${styles.btn} ${isModalOpen}`}
+          onClick={() => setModalOpen((prev) => !prev)}
         >
           <span></span>
           <span></span>
           <span></span>
         </div>
         <Link
-          className={styles.link}
+          className={`${styles.link} ${isActiveLink("/")}`}
           href="/"
-          onClick={() => setActive((prev) => !prev)}
+          onClick={() => setModalOpen((prev) => !prev)}
         >
           Home
         </Link>
         <Link
-          className={styles.link}
+          className={`${styles.link} ${isActiveLink("/projects")}`}
           href="/projects"
-          onClick={() => setActive((prev) => !prev)}
+          onClick={() => setModalOpen((prev) => !prev)}
         >
           Projects
         </Link>
         <Link
-          className={styles.link}
+          className={`${styles.link} ${isActiveLink("/about-me")}`}
           href="/about-me"
-          onClick={() => setActive((prev) => !prev)}
+          onClick={() => setModalOpen((prev) => !prev)}
         >
           About Me
         </Link>
