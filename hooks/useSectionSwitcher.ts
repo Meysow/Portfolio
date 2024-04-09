a9bfb2be496f1f5b5436ec32e8c358bf49bc06d1
Thibault D <thibault.dilman@tuta.io>
2024-04-09 11:01:55 +0200

    try with human approach

--- hooks/useSectionSwitcher.ts
+++ hooks/useSectionSwitcher.ts
@@ -23,32 +23,55 @@ const useSectionSwitcher = ({
   delayBetweenSectionChange,
 }: UseSectionSwitcherProps): UseSectionSwitcherReturn => {
   const [selectedSection, setSelectedSection] = useState<number>(1);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const lastScrollWheelTimestamp = useRef<number>(0);
+  const lastScrollWheelDelta = useRef<number>(0);
+  const animating = useRef<boolean>(false);
+
   const touchStartRef = useRef<number | null>(null);
   const touchEndRef = useRef<number | null>(null);
-  const lastInteractionRef = useRef<number>(0);
-  const sectionRef = useRef<HTMLDivElement>(null);
 
   const changeSection = useCallback(
     (increment: boolean): void => {
+      if (animating.current) return;
+
+      animating.current = true;
       setSelectedSection((prevSection) => {
         let newSection = increment ? prevSection + 1 : prevSection - 1;
         newSection = Math.max(1, Math.min(newSection, numberOfSections));
+
+        setTimeout(() => {
+          animating.current = false; // Reset animation lock after delay
+        }, delayBetweenSectionChange);
+
         return newSection;
       });
     },
-    [numberOfSections]
+    [numberOfSections, delayBetweenSectionChange]
   );
 
   const handleOnScroll = useCallback(
     (e: WheelEvent): void => {
-      const now = new Date().getTime();
-      if (now - lastInteractionRef.current < delayBetweenSectionChange) return;
-      lastInteractionRef.current = now;
+      const now = Date.now();
+      const minScrollWheelInterval = 300; // Minimum milliseconds between scrolls
+      const rapidSuccession =
+        now - lastScrollWheelTimestamp.current < minScrollWheelInterval;
+      const otherDirection = lastScrollWheelDelta.current > 0 !== e.deltaY > 0;
+      const speedDecrease =
+        Math.abs(e.deltaY) < Math.abs(lastScrollWheelDelta.current);
 
-      const scrollDown = e.deltaY > 0;
-      changeSection(scrollDown);
+      const isHuman = otherDirection || !rapidSuccession || !speedDecrease;
+
+      if (isHuman) {
+        const scrollDown = e.deltaY > 0;
+        changeSection(scrollDown);
+      }
+
+      lastScrollWheelTimestamp.current = now;
+      lastScrollWheelDelta.current = e.deltaY;
     },
-    [changeSection, delayBetweenSectionChange]
+    [changeSection]
   );
 
   const handleTouchStart = useCallback((e: TouchEvent): void => {
