f6ba770bcced02e5b8a9fd1c36247d0fcbf155f1
Thibault Dilman <thibault.dilman@protonmail.com>
2025-04-08 10:07:02 +0200

    Minor modif - removed commented lines

--- components/BackgroundImage/index.tsx
+++ components/BackgroundImage/index.tsx
@@ -1,19 +1,9 @@
-import style from "./BackgroundImage.module.scss";
+import style from './BackgroundImage.module.scss';
 
 const BackgroundImage = () => {
   return (
     <div className={style.bg_container}>
-      <div className={style.bg}>
-        {/* <Image
-          src={background}
-          alt="background image"
-          fill
-          priority={true}
-          placeholder="blur"
-          sizes="100vw"
-          quality={75}
-        /> */}
-      </div>
+      <div className={style.bg}></div>
     </div>
   );
 };
